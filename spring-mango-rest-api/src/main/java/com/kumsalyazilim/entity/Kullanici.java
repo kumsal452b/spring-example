163911ac9dd6f7421d4dbf8e1e1a52c8b7f200f0
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-08-31 21:14:40 +0300

    getter setter added

--- spring-mango-rest-api/src/main/java/com/kumsalyazilim/entity/Kullanici.java
+++ spring-mango-rest-api/src/main/java/com/kumsalyazilim/entity/Kullanici.java
@@ -9,6 +9,39 @@ public class Kullanici {
     @Id
     private String id;
     private String soyad,ad;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public HashMap getOzellikler() {
+        return ozellikler;
+    }
+
+    public void setOzellikler(HashMap ozellikler) {
+        this.ozellikler = ozellikler;
+    }
+
     private HashMap ozellikler;
 
 
